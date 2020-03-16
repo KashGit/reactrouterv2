f75e00092f0d3a32d5c7d56c79c1be965341727a
kashgit <kashif.nisar@gmail.com>
2020-03-16 12:46:25 +0530

    Dog List Added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom'
 import logo from './logo.svg';
-import './App.css';
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
+import whiskey from './img/whiskey.jpg'
+import hazel from './img/hazel.jpg'
+import tubby from './img/tubby.jpg'
+import DogList from './DogList';
 
-function App() {
+function App(props) {
+   
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Route exact path="/dogs" render = {() => <DogList dogs={props.dogs} />} />
+  )
 }
+App.defaultProps = {
+  dogs: [
+    {
+      name: "Whiskey",
+      age: 5,
+      src: whiskey,
+      facts: [
+        "Whiskey loves eating popcorn.",
+        "Whiskey is a terrible guard dog.",
+        "Whiskey wants to cuddle with you!"
+      ]
+    },
+    {
+      name: "Hazel",
+      age: 3,
+      src: hazel,
+      facts: [
+        "Hazel has soooo much energy!",
+        "Hazel is highly intelligent.",
+        "Hazel loves people more than dogs."
+      ]
+    },
+    {
+      name: "Tubby",
+      age: 4,
+      src: tubby,
+      facts: [
+        "Tubby is not the brightest dog",
+        "Tubby does not like walks or exercise.",
+        "Tubby loves eating food."
+      ]
+    }
+  ]
+};
 
 export default App;
---
+++ src/DogList.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './DogList.css'
+
+function DogList(props) {
+    return (
+        <div>
+            <h1 className='display-1 text-center'>Dog list</h1>
+            <div className='container'>
+                <div className='row'>
+                    {
+                        props.dogs.map(d => (
+                            <div className='dog col-md-4 text-center' key={d.name}>
+                                <img src={d.src} alt={d.name} />
+                                <h3>{d.name}</h3>
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
+
+DogList.propTypes = { dogs: PropTypes.object }
+
+export default DogList
+
